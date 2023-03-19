4b9b94552bde9c16b2a224623b1ba112b18b290a
Chao Cheng <ufo22940268@gmail.com>
2023-03-19 15:42:24 +0800

    Update readme and add a video demo.

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # auto-brightness
 
-Automatically adjust monitor brightness according the ambient light arround.
+Automatically adjust monitor brightness according the ambient light around.
+
+
+# Demo
+
+![](assets/IMG_3740.mov)
+
+# The materials you needs
+
+1. Raspberry Pi Pico
+2. BH1750FVI ambient light sensor
+3. Some wires used to connect Pico and light sensor
+4. An micro usb connector.
+
+# Build 
+
+1. Connect the GND and VCC ports from light sensor to pico
+2. Connect the SCL to GP14
+3. Connect the SDA to GP15
+4. Flash the `pico/main.py` into pico.
+
+# Install raycast extension
+
+It uses the raycast to run the sync job periodically to sync the brightness from pico into mac and then mac adjust the monitor brightness.
+So you have to install the raycast on you mac, and install this extension.
+
+After all this down, the sync background job should be started automatically as soon as raycast is launched.
---
+++ pico/main.py
@@ -0,0 +1,46 @@
+import time
+
+from machine import Pin, I2C, PWM
+
+i2c_2 = I2C(1, sda=Pin(14), scl=Pin(15), freq=400000)
+
+# Simple driver for the BH1750FVI digital light sensor
+
+MEASUREMENT_TIME = 120
+
+
+class BH1750FVI:
+    def __init__(self, i2c, addr=0x23, period=150):
+        self.i2c = i2c
+        self.period = period
+        self.addr = addr
+        self.time = 0
+        self.value = 0
+        self.i2c.writeto(addr, bytes([0x10]))  # start continuos 1 Lux readings every 120ms
+
+    def read(self):
+        self.time += self.period
+        if self.time >= MEASUREMENT_TIME:
+            self.time = 0
+            data = self.i2c.readfrom(self.addr, 2)
+            self.value = (((data[0] << 8) + data[1]) * 1200) // 1000
+        return self.value
+
+
+dev = BH1750FVI(i2c_2)
+
+pwm = PWM(Pin(25))
+
+# Set the PWM frequency.
+pwm.freq(1000)
+
+
+def update_led():
+    l = int(min(max(0, int(dev.read())), 200)/200*100)
+    print(l)
+    pwm.duty_u16(int(2 ** 16 * (l / float(200))))
+
+
+while True:
+    update_led()
+    time.sleep(0.1)
