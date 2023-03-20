3c4a24ab01df42ebbd78f29d95f43e60ea498eba
Chao Cheng <ufo22940268@gmail.com>
2023-03-20 15:25:32 +0800

    add max brightness.

---
+++ assets/test.py
@@ -0,0 +1,11 @@
+#!/Users/chao.cheng/.pyenv/shims/python3
+import serial
+import time
+
+ser = serial.Serial('/dev/tty.usbmodem21401', 9600, timeout=1)  # 打开串口
+ser.flushInput()
+while True:
+    data = ser.readline()
+    print(data)
+    time.sleep(1)
+# ser.close()
