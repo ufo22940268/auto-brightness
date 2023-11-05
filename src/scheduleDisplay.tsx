462c463729db3ed40442915e198a83f123709b5a
Chao Cheng <ufo22940268@gmail.com>
2023-11-05 17:00:24 +0800

    v

--- src/scheduleDisplay.tsx
+++ src/scheduleDisplay.tsx
@@ -4,9 +4,9 @@ import { hasChange } from "./temporaryChangeDisplay";
 export default async () => {
   const currentHour = new Date().getHours();
 
-  if (hasChange() != null) {
-    return true;
-  }
+  // if (hasChange() != null) {
+  //   return true;
+  // }
 
   if (currentHour >= 21 || currentHour < 6) {
     console.log('turnOffDisplay');
--- src/syncBrightness.tsx
+++ src/syncBrightness.tsx
@@ -2,6 +2,7 @@ import { isNumeric, readBrightness } from "./sensor";
 import { $ } from "execa";
 import { Cache, getPreferenceValues, launchCommand, LaunchType, showHUD } from "@raycast/api";
 
+const cache = new Cache();
 function clampBrightness(brightness: number) {
     const maxBrightness = getPreferenceValues()["maxBrightness"]
     if (!isNumeric(maxBrightness)) {
@@ -15,18 +16,20 @@ function clampBrightness(brightness: number) {
 }
 
 async function setBrightnessToMonitor(brightness: number) {
-    const {stdout: current} = await $`/usr/local/bin/m1ddc get luminance`
-    if (isNumeric(current) && Math.abs(Number(current) - brightness) < 10) {
-        console.log(`current monitor brightness: ${current}, ambient brightness: ${brightness}. no need to update brightness`);
-        return;
+    const current = cache.get("brightness");
+    if (current != undefined && isNumeric(current)) {
+        if (isNumeric(current) && Math.abs(Number(current) - brightness) < 10) {
+            console.log(`current monitor brightness: ${current}, ambient brightness: ${brightness}. no need to update brightness`);
+            return;
+        }
+
+        if (Number(current) < 0) {
+            console.log("failed to get current brightness of monitor: ", +current);
+            return;
+        }
     }
 
-    if (Number(current) < 0) {
-        console.log("failed to get current brightness of monitor: ", +current);
-        return;
-    }
-
-    console.log("update brightness");
+    console.log("update current brightness " + current + "  to " + brightness);
     await $`/usr/local/bin/m1ddc set luminance ${brightness}`;
     if (getPreferenceValues()["promptBrightnessChange"]) {
         await showHUD(`adjust brightness ${current} to ${brightness}`);
@@ -37,8 +40,10 @@ async function setBrightnessToMonitor(brightness: number) {
 export default async () => {
     let brightness = await readBrightness();
     console.log("start syncing, sensor brightness: " + JSON.stringify(brightness, null, 2));
-    brightness = clampBrightness(brightness)
-    const newBrightness = await setBrightnessToMonitor(brightness)
-    if (newBrightness != undefined) new Cache().set("brightness", newBrightness.toString());
+    brightness = clampBrightness(brightness);
+    const newBrightness = await setBrightnessToMonitor(brightness);
+    if (newBrightness != undefined) {
+        cache.set("brightness", newBrightness.toString());
+    }
     await launchCommand({ name: "brightnessDiameter", type: LaunchType.Background });
 }
