343cb34ae76af7f2eeed4b50719d2e5d8e093ce1
Chao Cheng <ufo22940268@gmail.com>
2024-09-12 12:41:56 +0800

    test

---
+++ assets/send_text.py
@@ -0,0 +1,14 @@
+import time
+
+import serial
+
+# 创建一个 serial 对象
+# 注意，你需要将 'COMx' 替换为你的设备的正确端口号
+ser = serial.Serial('/dev/cu.usbmodem21401', 9600)
+
+# 向 Pico 发送文本
+ser.write(b'\x7B')
+print('send--')
+
+# 关闭串口
+ser.close()
