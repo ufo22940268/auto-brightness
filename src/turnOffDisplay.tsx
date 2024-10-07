e79014348a5c63950981a3ecb94d0bed6a5da578
Chao Cheng <ufo22940268@gmail.com>
2024-10-08 04:18:27 +0800

    add turn off display feature.

--- src/turnOffDisplay.tsx
+++ src/turnOffDisplay.tsx
@@ -3,8 +3,10 @@ import { turnOffDisplay, turnOnDisplay } from "./sensor";
 export default async () => {
   const currentHour = new Date().getHours();
   if (currentHour >= 21 || currentHour < 6) {
+    console.log('turnOffDisplay');
     await turnOffDisplay();
   } else {
+    console.log('turnOnDisplay');
     await turnOnDisplay();
   }
 }
